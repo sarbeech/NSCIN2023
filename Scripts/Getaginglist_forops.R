aad0cca3033540b79d1d3cf470e120a7c666dd1a
AlexRowMNRF <alex.row@ontario.ca>
2023-12-19 11:13:45 -0500

    Update code to create a function to automate aging lists for ops

--- Scripts/Getaginglist_forops.R
+++ Scripts/Getaginglist_forops.R
@@ -2,7 +2,9 @@
 #then pull out those fish from the fn125 in an excel sheet for agers to enter ages into (along with other fields)
 
 library(RODBC)
+library(glfishr)
 #pull in from access database
+src_db <- "//cihs.ad.gov.on.ca/MNRF/Groups/LEGACY/LRCPGLENFP00001/FWSB_Glenora/Information Resources/Field Season/2023/LOA_IA23_NSI.accdb"
 src_db <- "//cihs.ad.gov.on.ca/MNRF/Groups/LEGACY/LRCPGLENFP00001/FWSB_Glenora/Information Resources/Field Season/2023/LOA_IA23_NSF.accdb"
 
 fetch_data <- function(table, prj_cd, src_db){
@@ -18,74 +20,58 @@ library(dplyr)
 ages <- fn125 %>% 
   group_by(SPC,AGEST) %>%
   dplyr::summarize(number=n(),
-                   sizemin=min(FLEN))
-
-
+                   sizemin=min(FLEN));ages
 #get entry sheets
 fn125 <- fn125 %>% 
   filter(!is.na(AGEST))
 
-fn125 %>% 
-  group_by(SPC, AGEST) %>% 
-  summarise( n = n()) #154 scales, 55 otoliths, 3 pectoral spines, 25 operculums
-
-
-
 library(writexl)
+#CCreate function to get aging list 
+#NOTE THIS FUNCTION DOES NOT SEPARATE AND CREATE MULTIPLE FILES IF MULTIPLE AGING STRUCTURES ARE PRESENT
+# MUST CREATE COPIES AND RENAME THEM TO GET DIFFERENT FIELS FOR CONTRACTOR -- for example pike require 3 files since there are clithrum, scales and anal fin
+getAgingLists <- function(SPC_Code, data){
+  for (i in 1:length(SPC_Code)){
+    dat <- data %>% 
+      filter(SPC == SPC_Code[1])
+    write_xlsx(dat, paste0("./Data/Ageing/", dat$spc_nmco[1], ".", substr(dat$PRJ_CD[1], nchar(dat$PRJ_CD[1]) - 2, nchar(dat$PRJ_CD[1])), dat$AGEST[1], ".xlsx"))
+    
+  }
+}
 
-#select species of interest to age 
+#select species of interest to age for LSF
+#Note -- need to change src_db to connect to the correct project database 
 
 lsf_aging23 <- fn125 %>% 
-  filter(SPC %in% c("131", "314", "316", "317", "331", "334", "483", "172", "168", "171", "319")) %>% 
+  filter((SPC %in% c("131", "314", "316", "317", "331", "334", "483", "172", "168", "171", "319") & !(SPC == "319" & AGEST == "2")))  %>% #removed scales from crappie since only aging otoliths
   mutate(AGEID="",PREFERRED="",AGEA="",AGEMT="",EDGE="",CONF="",NCA="",COMMENT7="") %>% 
   select(PRJ_CD, SAM, EFF, GRP, SPC, FISH, AGEMT, AGEID, NCA, EDGE, CONF, AGEA, PREFERRED, COMMENT7, AGEST)
-lsf_aging23$AGEST <- gsub("28d", "28D", lsf_aging23$AGEST)
+lsf_aging23$AGEST <- gsub("28d", "28D", lsf_aging23$AGEST) #fix typo
 
-#pike
-pike <- lsf_aging23 %>%
-  filter(SPC=="131")
+SPC_full_name <- get_species(list(spc = lsf_aging23$SPC, detail = TRUE), to_upper = FALSE) #get species name
+SPC_full_name <- SPC_full_name %>% 
+  select(spc, spc_nmco) %>% 
+  rename(SPC =  spc) #rename columsn for join
+lsf_aging23 <- left_join(lsf_aging23, SPC_full_name) #add species name to data
 
-write_xlsx(pike,"./Data/Ageing/pike_NSF_cleithrum_Contractor.xlsx")
-write_xlsx(pike,"./Data/Ageing/pike_NSF_Scale_Contractor.xlsx")
-write_xlsx(pike,"./Data/Ageing/pike_NSF_AnalFin.xlsx")
+SPC_codes <- unique(lsf_aging23$SPC) #create vector of species for aging 
+getAgingLists(SPC_Code = SPC_codes, data = lsf_aging23)
 
-#walleye
-walleye <- lsf_aging23 %>%
-  filter(SPC=="334")
-write_xlsx(walleye,"./Data/Ageing/Walleye_NSF_otoliths.xlsx")
+###################################################################################################################
+#select species of interest to age for NSI 
+#make sure to switch src_db to access the correct database for NSI
+NSI_aging23 <- fn125 %>% 
+  filter(SPC %in% c("131", "314", "316", "317", "331", "334", "319")) %>% 
+  mutate(AGEID="",PREFERRED="",AGEA="",AGEMT="",EDGE="",CONF="",NCA="",COMMENT7="") %>% 
+  select(PRJ_CD, SAM, EFF, GRP, SPC, FISH, AGEMT, AGEID, NCA, EDGE, CONF, AGEA, PREFERRED, COMMENT7, AGEST)
+SPC_full_name <- get_species(list(spc = NSI_aging23$SPC, detail = TRUE), to_upper = FALSE)
+SPC_full_name <- SPC_full_name %>% 
+  select(spc, spc_nmco) %>% 
+  rename(SPC =  spc)
+NSI_aging23 <- left_join(NSI_aging23, SPC_full_name)
 
-#yellow perch
-Yperch <- lsf_aging23 %>%
-  filter(SPC=="331")
-write_xlsx(Yperch,"./Data/Ageing/YellowPerch_NSF.xlsx")
+SPC_codes <- unique(NSI_aging23$SPC)
 
-#bluegill
-bluegill <- lsf_aging23 %>%
-  filter(SPC=="314")
-write_xlsx(bluegill,"./Data/Ageing/bluegill_NSF.xlsx")
-
-#smallmouth
-SMB <- lsf_aging23 %>%
-  filter(SPC=="316")
-write_xlsx(SMB,"./Data/Ageing/Smallmouth_NSF.xlsx")
-
-#largemouth
-LMB <- lsf_aging23 %>%
-  filter(SPC=="317")
-write_xlsx(LMB,"./Data/Ageing/Largemouth_NSF.xlsx")
-
-#moxastoma sp. 
-moxa <- lsf_aging23 %>%
-  filter(SPC %in% c("168", "171", "172"))
-write_xlsx(moxa,"./Data/Ageing/Moxastoma_NSF.xlsx")
-
-#tench 
-tench <- lsf_aging23 %>%
-  filter(SPC=="483")
-write_xlsx(tench,"./Data/Ageing/tench_NSF_otolith.xlsx")
-
-#crappie
-crappie <- lsf_aging23 %>%
-  filter(SPC=="319")
-write_xlsx(crappie,"./Data/Ageing/crappie_NSF_otolith.xlsx")
-
+# Call the function with SPC_codes
+#NOTE THIS FUNCTION DOES NOT SEPARATE AND CREATE MULTIPLE FILES IF MULTIPLE AGING STRUCTURES ARE PRESENT
+# MUST CREATE COPIES AND RENAME THEM TO GET DIFFERENT FIELS FOR CONTRACTOR
+getAgingLists(SPC_codes, NSI_aging23)
